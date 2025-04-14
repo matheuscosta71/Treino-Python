d1f7c608c3fa326cbdf49df8567c63029a5f7b38
matheuscosta71 <matheushenri26@outlook.com>
2025-04-14 16:23:59 -0300

    Inicializando repositório!

---
+++ app.py
@@ -0,0 +1,11 @@
+print("""Sabor Express 
+      \n""")
+
+print ('1. Cadastrar Restaurante')
+print ('2. Listar Restaurante')
+print ('3. Ativar Restaurante')
+print ('4. Sair')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print (f'Você escolheu a opção {opcao_escolhida}')
+
